f88ffdc1519954f21b604c7e1895a5f124c7d685
thmslmr <thomas.lemeur@edu.esiee.fr>
2018-03-16 15:50:58 +0100

    Add new function to generate anchors faster

--- lib/layer_utils/generate_anchors.py
+++ lib/layer_utils/generate_anchors.py
@@ -110,6 +110,31 @@ def _scale_enum(anchor, scales):
     return anchors
 
 
+def generate_anchors_fast(base_size=16, ratios=np.array([.5, 1, 2]),
+                          scales=np.array([8., 16., 32.])):
+
+    # Get the number of anchors & Initialize anchors matrix
+    num_anchors = ratios.size * scales.size
+    anchors = np.zeros((num_anchors, 4))
+
+    # Scale base_size
+    scales_base = np.tile(scales, (2, ratios.size)).T
+    anchors[:, 2:] = base_size * scales_base
+
+    # Compute areas of anchors w * h
+    areas = anchors[:, 2] * anchors[:, 3]
+
+    # Apply ratios on areas & correct height
+    anchors[:, 2] = np.sqrt(areas / np.repeat(ratios, scales.size))
+    anchors[:, 3] = anchors[:, 2] * np.repeat(ratios, scales.size)
+
+    # From center, w,h to corners
+    anchors[:, 0::2] -= np.tile(anchors[:, 2] / 2, (2, 1)).T
+    anchors[:, 1::2] -= np.tile(anchors[:, 3] / 2, (2, 1)).T
+
+    return np.round(anchors)
+
+
 if __name__ == '__main__':
     import time
 

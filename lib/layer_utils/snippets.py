8d03d3fbb8f0f79bb1ce6368d325fccc573ed901
thmslmr <thomas.lemeur@edu.esiee.fr>
2018-03-16 09:36:54 +0100

    generate anchors with tf

--- lib/layer_utils/snippets.py
+++ lib/layer_utils/snippets.py
@@ -10,20 +10,30 @@ from __future__ import print_function
 import numpy as np
 from layer_utils.generate_anchors import generate_anchors
 
-def generate_anchors_pre(height, width, feat_stride, anchor_scales=(8,16,32), anchor_ratios=(0.5,1,2)):
-  """ A wrapper function to generate anchors given different scales
-    Also return the number of anchors in variable 'length'
-  """
-  anchors = generate_anchors(ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
-  A = anchors.shape[0]
-  shift_x = np.arange(0, width) * feat_stride
-  shift_y = np.arange(0, height) * feat_stride
-  shift_x, shift_y = np.meshgrid(shift_x, shift_y)
-  shifts = np.vstack((shift_x.ravel(), shift_y.ravel(), shift_x.ravel(), shift_y.ravel())).transpose()
-  K = shifts.shape[0]
-  # width changes faster, so here it is H, W, C
-  anchors = anchors.reshape((1, A, 4)) + shifts.reshape((1, K, 4)).transpose((1, 0, 2))
-  anchors = anchors.reshape((K * A, 4)).astype(np.float32, copy=False)
-  length = np.int32(anchors.shape[0])
+def generate_anchors_pre(height, width, feat_stride=16,
+    anchor_scales=(8,16,32), anchor_ratios=(0.5,1,2)):
+    """
+        A wrapper function to generate anchors given different scales
+        Also return the number of anchors in variable 'length'
+    """
 
-  return anchors, length
+    shift_x = tf.range(width) * feat_stride
+    shift_y = tf.range(height) * feat_stride
+    shift_x, shift_y = tf.meshgrid(shift_x, shift_y)
+
+    sx = tf.reshape(shift_x, shape=(-1,))
+    sy = tf.reshape(shift_y, shape=(-1,))
+
+    anchors = generate_anchors(ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
+
+    A = anchors.shape[0]
+    K = tf.multiply(width, height)
+    length = K * A
+
+    shifts = tf.transpose(tf.stack([sx, sy, sx, sy]))
+    shifts = tf.transpose(tf.reshape(shifts, shape=[1, K, 4]), perm=(1, 0, 2))
+
+    anchor_constant = tf.constant(anchors.reshape((1, A, 4)), dtype=tf.int32)
+    anchors_tf = tf.reshape(tf.add(anchor_constant, shifts), shape=(length, 4))
+
+    return tf.cast(anchors_tf, dtype=tf.float32), length
